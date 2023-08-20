5cb03182b0bf455274a921366bc6379fca5a6df8
akshay palase <akshaypalase3@gmail.com>
2023-08-20 21:29:33 +0530

    class inheritance added in class.js

--- class.js
+++ class.js
@@ -1,4 +1,4 @@
-//creating a class with properties and methods
+//1 creating a class with properties and methods
 class Car{
     constructor(name,id){
        this.name=name,
@@ -12,4 +12,36 @@ class Car{
 
 bmw=new Car('BMW',101)
 bmw.start();
+
+// how to implement class inheritance 
+
+class Car1{
+    constructor(name,model){
+        this.name=name,
+        this.model=model
+    }
+
+    start(){
+         console.log(this.name,this.model);
+    }
+    end(){
+        console.log('car stopped');
+    }
+}
+
+class ElectricCar extends Car1{
+
+    dashboard(){
+        console.log('child class is called');
+    }
+    start(){
+        super.start();
+        super.end();
+        this.dashboard()
+    }
+}
+
+evcar=new ElectricCar('Tesla','AWDv34');
+evcar.start();
+
 
